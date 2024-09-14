a78d3ab82e5cf06592bd721b2f14c2343d41049a
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-09-13 20:59:34 -0500

    Added figures

---
+++ Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Progra1Semana1.Figures
+{
+    internal abstract class Shape
+    {
+        protected string name;
+
+        public abstract float GetArea();
+
+        public virtual string GetData()
+        {
+            return name;
+        }
+
+    }
+}
